16e4a821b7026f93f3e3fab94fdf9b10f4699a2a
yuto.haga <yuto.haga@au-cl.co.jp>
2020-01-30 16:17:16 +0900

    4-1

---
+++ pass_array.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/*
+配列の先頭ポインタをもらうが、その配列がいくつ(要素数)あるのか？
+わからないため、sizeももらう必要がある。
+*/
+void func(int *array, int size) {
+  int i;
+
+  for (i = 0; i < size; i++) {
+    printf("array[%d]..%d\n", i, array[i]);
+  }
+}
+
+int main(void) {
+  int array[] = {1, 2, 3, 4, 5};
+  func(array, sizeof(array) / sizeof(array[0]));
+  return 0;
+}
