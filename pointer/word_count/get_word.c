94f353dfbcf5dc64b327b9294def9c434f33af2b
HagaSpa <justicesparrow13@gmail.com>
2020-03-13 22:06:59 +0900

    create word_count

---
+++ pointer/word_count/get_word.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+
+
+/*
+ * ファイルを読み込んで、１つずつ単語を返却する。
+ * 
+ */
+int getword(char *buf, int buf_size, FILE *fp)
+{
+    int len;
+    int ch;
+
+    /* 空白文字を読み飛ばす */
+    while ((ch = getc(fp)) != EOF && !isalnum(ch));
+    
+    /* ファイルの最後ならEOFを返却 */
+    if (ch == EOF) return EOF;
+
+    /* chには単語の最初の文字が格納されているはず */
+    len = 0;
+    do {
+        buf[len] = ch;
+        len++;
+        if (len >= buf_size) {
+            fprintf(stderr, "word too long.\n");
+            exit(1);
+        }
+    } while ((ch = getc(fp)) != EOF && isalnum(ch));
+
+    buf[len] = '\0';
+    return len;
+}
+
+/* Test Method*/
+int main(void) 
+{
+    char buf[256];
+
+    while (getword(buf, 256, stdin) != EOF) {
+        printf("<<%s>>\n", buf);
+    }
+    
+    return 0;
+}
