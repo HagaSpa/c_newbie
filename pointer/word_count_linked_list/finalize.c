f7a76dab28199f2d3eb3e40378877ff267c1ca4c
HagaSpa <justicesparrow13@gmail.com>
2020-03-15 13:57:16 +0900

    Create finalize.c

---
+++ pointer/word_count_linked_list/finalize.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include "word_manage_p.h"
+
+/**
+ * 単語管理部の終了処理を行う
+ */
+void word_finalize(void)
+{
+    Word *temp;
+
+    while (word_header != NULL) {
+        temp = word_header;
+        word_header = word_header->next;
+        free(temp->name);
+        free(temp);
+    }
+}
